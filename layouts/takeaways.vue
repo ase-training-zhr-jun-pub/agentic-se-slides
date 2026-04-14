551809c04794f62692c7a333a4c5e4a8cb811ff0
Michael van Engelshoven <michael@van-engelshoven.de>
2026-04-14 11:36:33 +0200

    Refine takeaways layout spacing

--- layouts/takeaways.vue
+++ layouts/takeaways.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Center from 'slidev-theme-innoq/layouts/center.vue'
-import VisualCenter from 'slidev-theme-innoq/components/VisualCenter.vue'
 
 const props = withDefaults(defineProps<{
   background?: string
@@ -14,21 +13,12 @@ const props = withDefaults(defineProps<{
 
 <template>
   <Center :background="props.background">
-    <div class="h-full w-full grid grid-rows-[1fr_auto] text-center">
-      <VisualCenter>
-        <template #top>
-          <h3 class="[&_em]:text-apricot">
+      <div class="[&_ol]:inline-block [&_ol]:max-w-3xl [&_ol]:list-decimal [&_ol]:pl-8 [&_ol]:text-left [&_ol]:text-2xl [&_ol]:leading-snug [&_ol]:space-y-3 [&_ol>li::marker]:text-accent">
+        <h3 class="mb-0 [&+h1,h2,h3,h4,h5,h6]:mb-10">
             <em>Chapter {{ props.chapter }}</em>
-          </h3>
-        </template>
-        <h1 class="text-white !mb-0">{{ props.title }}</h1>
-      </VisualCenter>
-
-      <div class="w-full flex items-center justify-center mb-16">
-        <div class="[&_ol]:inline-block [&_ol]:max-w-3xl [&_ol]:list-decimal [&_ol]:pl-8 [&_ol]:text-left [&_ol]:text-2xl [&_ol]:leading-snug [&_ol]:space-y-3 [&_ol>li::marker]:text-accent">
-          <slot />
-        </div>
-      </div>
+        </h3>
+        <h1>{{ props.title }}</h1>
+        <slot />
     </div>
   </Center>
 </template>
